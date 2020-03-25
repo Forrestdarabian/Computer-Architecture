0b9fb8857b4e99d3e82365695d08ffafd41a6f95
Forrest-Darabian <mr.smiley72@yahoo.com>
2020-03-25 14:43:04 -0700

    day 3 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,6 +6,9 @@ LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
 MUL = 0b10100010
+PUSH = 0b01000101
+POP = 0b01000110
+SP = 7
 
 
 class CPU:
@@ -13,14 +16,61 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 8
+        self.ram = [0] * 256
         self.register = [0] * 8
         self.pc = 0
+        self.running = True
+        self.branch_table = {
+            HLT: self.HLT_op,
+            LDI: self.LDI_op,
+            PRN: self.PRN_op,
+            MUL: self.MUL_op,
+            PUSH: self.PUSH_op,
+            POP: self.POP_op
+        }
+
+    def HLT_op(self, oper_a, oper_b):
+        self.running = False
+
+    def LDI_op(self, oper_a, oper_b):
+        self.reg[oper_a] = oper_b
+        self.pc += 3
+
+    def PRN_op(self, oper_a, oper_b):
+        print(self.reg[oper_a])
+        self.pc += 2
+
+    def MUL_op(self, oper_a, oper_b):
+        self.alu('MUL', oper_a, oper_b)
+        self.pc += 3
+
+    def PUSH_op(self, oper_a, oper_b):
+        self.push(self.reg[oper_a])
+        self.pc += 2
+
+    def POP_op(self, oper_a, oper_b):
+        self.reg[oper_a] = self.pop()
+        self.pc += 2
+
+    def push(self, value):
+        self.reg[SP] -= 1
+        self.ram_write(value, self.reg[7])
+
+    def pop(self):
+        value = self.ram_read(self.reg[7])
+        self.reg[SP] += 1
+        return value
+
+    def ram_read(self, address):
+        return self.ram[int(str(address), 2)]
+
+    def ram_write(self, address, value):
+        self.ram[int(str(address), 2)] = value
 
     def load(self, filename):
         """Load a program into memory."""
 
-        # address = 0
+        address = 0
 
         # # For now, we've just hardcoded a program:
 
@@ -44,17 +94,17 @@ class CPU:
             with open(sys.argv[1]) as f:
                 # Read all the lines
                 for line in f:
-                     # Parse out the comments
-                     comment_split = line.strip().split("#")
-                      # cast the numbers from strings to ints
-                      value = comment_split[0].strip()
-                       # ignore blank lines
-                       if value == "":
-                            continue
-                        instruction = int(value, 2)
-                        # populate a memory array
-                        self.ram[address] = instruction
-                        address += 1
+                    # Parse out the comments
+                    comment_split = line.strip().split("#")
+                    # cast the numbers from strings to ints
+                    value = comment_split[0].strip()
+                    # ignore blank lines
+                    if value == "":
+                        continue
+                    instruction = int(value, 2)
+                    # populate a memory array
+                    self.ram[address] = instruction
+                    address += 1
 
         except FileNotFoundError:
             print("File not found")
@@ -94,29 +144,15 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        IR = self.pc
-        running = True
-
-        while running:
-            opcode = self.ram_read(IR)
-            operand_a = self.ram_read(IR + 1)
-            operand_b = self.ram_read(IR + 2)
-            if opcode == LDI:
-                self.reg[operand_a] = operand_b
-                IR += 3
-            elif opcode == PRN:
-                print(self.reg[operand_a])
-                IR += 2
-            elif opcode == MUL:
-                self.alu(opcode, operand_a, operand_b)
-                IR += 3
-            elif opcode == HLT:
-                sys.exit(0)
+        while self.running:
+            IR = self.ram_read(self.pc)
+            oper_a = self.ram_read(self.pc + 1)
+            oper_b = self.ram_read(self.pc + 2)
+            if int(bin(IR), 2) in self.branch_table:
+                self.branch_table[IR](oper_a, oper_b)
             else:
-                print("OPCODE not recognized.")
-                sys.exit(1)
-
-
+                raise Exception(
+                    f'Invalid {IR}, not in branch table \t {list(self.branch_table.keys())}')
 
             # # LDI
             # if self.ram[self.pc] == 0b10000010:
@@ -133,9 +169,3 @@ class CPU:
             # elif self.ram[self.pc] == 0b00000001:
             #     self.pc = 0
             #     running = False
-
-    def ram_read(self, address):
-        return self.ram[int(str(address), 2)]
-
-    def ram_write(self, address, value):
-        self.ram[int(str(address), 2)] = value
